03f2b7cfc35e82b8c3f4ad93482fe950f54d671f
sai-Sweshika <saisweshikakodurupaka@gmail.com>
2024-11-03 18:30:58 +0530

    first commit

---
+++ js.js
@@ -0,0 +1,56 @@
+// switch ("Sunday") {
+//     case "Monday":
+//         console.log("Its monday")
+//         break;
+//     case "Tuesday":
+//         console.log("Its tuesday")
+//         break;
+//     case "Wednesday":
+//         console.log("Its wednesday")
+//         break;
+//     case "Thursday":
+//         console.log("Its thursday")
+//         break;
+//     case "Friday":
+//         console.log("Its Friday")
+//         break;
+//     case "Saturday":
+//         console.log("Its saturday")
+//         break;
+//     case "Sunday":
+//         console.log("Its sunday")
+//         break;
+//     default:
+//         console.log("It was not a weekdays")
+// }
+// o/p:sunday
+
+// var a=[1,2,3,4,5]
+// for(var number of a){
+//     console.log(number)
+// }
+// o/p: 1
+//      2
+//      3
+//      4
+//      5
+
+
+// var a=["sai","sweshika","rajakumari","srinivas"]
+// for(var element of a){
+//     console.log(element)
+// }
+// o/p: sai 
+//      sweshika
+//      rajakumari
+//      srinivas
+
+//  var a="sweety"
+//  for (var char of a){
+//     console.log(char)
+//  }
+//  o/p: s 
+//       w 
+//       e 
+//       t 
+//       y
